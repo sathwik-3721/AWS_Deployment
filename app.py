c7d67f7dfc477d674bb1301394e7f5859875d688
Sathwik Ramisetty <sathik3721@gmail.com>
2024-01-02 14:32:40 +0530

    AWS Deploy

---
+++ app.py
@@ -0,0 +1,36 @@
+# Import the dependencies
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+# Create an object for Flask
+app = Flask(__name__)
+
+# First let's open pickle file
+with open('House_Price.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+@app.route('/', methods = ['GET'])
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    Rooms = int(request.form['bedrooms'])
+    Bathrooms = int(request.form['bathrooms'])
+    Place = int(request.form['location'])
+    Area = int(request.form['area'])
+    Status = int(request.form['status'])
+    Facing = int(request.form['facing'])
+    P_Type = int(request.form['type'])
+
+    # Now take the above data and convert it into array
+    input_data = np.array([[Place, Area, Status, Rooms, Bathrooms, Facing, P_Type]])
+
+    # Pass the above data to the model for the prediction
+    prediction = model.predict(input_data)[0] # (input_data)[0] --> [0] is placed because it is a 2-d array because sklearn needs 2-d array
+
+    # Now pass the above predicted data to the template
+    return render_template('index.html', prediction = prediction)
+
+app.run()
